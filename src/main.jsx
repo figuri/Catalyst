5d24536cdb2ccdc547f5028f2c3fa0953d4a53d1
unknown <samtisme@gmail.com>
2024-01-15 12:20:17 -0800

    finished main.jsx added paths for different page components

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app.jsx";
+import "./index.css";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Jaelen from './components/pages/Jaelen.jsx'
-import Home from './components/pages/Home.jsx'
-import Josiah from './components/pages/Josiah.jsx'
-import Caleb from './components/pages/Caleb.jsx'
-import Samuel from './components/pages/Samuel.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Jaelen from "./components/pages/Jaelen.jsx";
+import Home from "./components/pages/Home.jsx";
+import Josiah from "./components/pages/Josiah.jsx";
+import Caleb from "./components/pages/Caleb.jsx";
+import Samuel from "./components/pages/Samuel.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/samuel",
+        element: <Samuel />,
+      },
+      {
+        path: "/josiah",
+        element: <Josiah />,
+      },
+      {
+        path: "/caleb",
+        element: <Caleb />,
+      },
+      {
+        path: "/jaelen",
+        element: <Jaelen />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
