1016a8c7f3c5bb4eef80cab35c4f9a3aec2d7298
unknown <samtisme@gmail.com>
2024-01-14 23:29:56 -0800

    adding main.jsx variables and imports

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './app.jsx'
+import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Jaelen from './components/pages/Jaelen.jsx'
+import Home from './components/pages/Home.jsx'
+import Josiah from './components/pages/Josiah.jsx'
+import Caleb from './components/pages/Caleb.jsx'
+import Samuel from './components/pages/Samuel.jsx'
